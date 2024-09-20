631cc11b391078f5779285625503c5535161df81
davidebrig <66873218+davidebrig@users.noreply.github.com>
2024-09-20 15:23:48 +0200

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-// config.js
-
-const CONFIG = {
-    APPS_SCRIPT_WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbxcCLGiMzD-Qn7NSsnXc8OA3Ocw5tTLZPKypqq5c5Nr8fTaTaHof30sgK1FZraahehn/exec',  // Inserisci l'URL del Google Apps Script Web App
-    SPREADSHEET_ID: '1qC2t3XW19Wg5olWE6rKA7aDBkuRvN-x9TOR7Q4QKcow'  // Inserisci l'ID dello Spreadsheet
-};
