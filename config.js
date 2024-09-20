3e5c7385bf1bb2821ba794e1d29f5bdb6a89c90a
davidebrig <66873218+davidebrig@users.noreply.github.com>
2024-09-20 14:36:35 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+// config.js
+
+const CONFIG = {
+    APPS_SCRIPT_WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbwPb2j1jdDHYm579xVNowNXLTE7tbpYHy2w55-s4Ae_UPc5lSDxdsSv4eetmOkmrNPi/exec',  // Inserisci l'URL del Google Apps Script Web App
+    SPREADSHEET_ID: '1qC2t3XW19Wg5olWE6rKA7aDBkuRvN-x9TOR7Q4QKcow'  // Inserisci l'ID dello Spreadsheet
+};
