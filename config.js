1872eb572236206c1f823b86cd632071fc0defb3
davidebrig <66873218+davidebrig@users.noreply.github.com>
2024-09-20 14:48:10 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // config.js
 
 const CONFIG = {
-    APPS_SCRIPT_WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbwPb2j1jdDHYm579xVNowNXLTE7tbpYHy2w55-s4Ae_UPc5lSDxdsSv4eetmOkmrNPi/exec',  // Inserisci l'URL del Google Apps Script Web App
+    APPS_SCRIPT_WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbxI_W9J7GrOP59P_oQQnEt2sc7rTV8M6Ncut1f1_93J90un3OK8MjPSkC6tcJrflWie/exec',  // Inserisci l'URL del Google Apps Script Web App
     SPREADSHEET_ID: '1qC2t3XW19Wg5olWE6rKA7aDBkuRvN-x9TOR7Q4QKcow'  // Inserisci l'ID dello Spreadsheet
 };
